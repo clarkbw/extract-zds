a4a0ff1d0ce4550ae6f219ef2b71cd7d037972be
Bryan Clark <clarkbw@github.com>
2020-06-16 09:33:36 -0700

    Fixup tests and dedupe returns

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -10,21 +10,38 @@ process.env['INPUT_TOKEN'] = 'TOKEN';
 process.env['GITHUB_REPOSITORY'] = 'github/c2c-packages';
 process.env['GITHUB_EVENT_PATH'] = path.join(__dirname, 'payload.json');
 
-import {zeds} from '../src/zeds';
+import {zeds, bodies, query} from '../src/zeds';
 
-// jest.doMock('@actions/github', () => {
-//   return {
-//     GitHub: (token: string) => {
-//       console.log(`token: ${token}`);
-//       return {
-//         issues: {
-//           get: jest.fn(() => issue),
-//           listComments: jest.fn(() => comments)
-//         }
-//       };
-//     }
-//   };
-// });
+test('get all zeds', async () => {
+  const github = require('@actions/github');
+
+  nock.disableNetConnect();
+  nock('https://api.github.com:443')
+    .get('/repos/github/c2c-packages/issues/12345')
+    .reply(200, issue);
+
+  nock('https://api.github.com:443')
+    .get('/repos/github/c2c-packages/issues/12345/comments')
+    .reply(200, comments.data);
+
+  const b = await query();
+  expect(b.length).toBe(8);
+});
+
+test('get all the bodies', async () => {
+  const github = require('@actions/github');
+
+  nock.disableNetConnect();
+  nock('https://api.github.com:443')
+    .get('/repos/github/c2c-packages/issues/12345')
+    .reply(200, issue);
+
+  nock('https://api.github.com:443')
+    .get('/repos/github/c2c-packages/issues/12345/comments')
+    .reply(200, comments.data);
+  const b = await bodies(new github.GitHub('TOKEN'));
+  expect(b.length).toBe(6);
+});
 
 test('run main', async () => {
   nock.disableNetConnect();
@@ -37,12 +54,3 @@ test('run main', async () => {
     .reply(200, comments.data);
   await zeds();
 });
-
-// // shows how the runner will run a javascript action with env / stdout protocol
-// test('test runs', () => {
-//   const ip = path.join(__dirname, '..', 'lib', 'main.js');
-//   const options: cp.ExecSyncOptions = {
-//     env: process.env
-//   };
-//   console.log(cp.execSync(`node ${ip}`, options).toString());
-// });
--- src/zeds.ts
+++ src/zeds.ts
@@ -1,31 +1,15 @@
 import * as core from '@actions/core';
 import * as github from '@actions/github';
+import {type} from 'os';
+
+// export const ZD_REGEX = /ZD-(\d+)|github\.zendesk\.com\/agent\/tickets\/(\d+)/;
+export const ZD_REGEX = /ZD-(\d+)/g;
+export const ZD_URL_REGEX = /github\.zendesk\.com\/agent\/tickets\/(\d+)/g;
 
 export async function zeds(): Promise<void> {
   try {
-    const token = core.getInput('token', {required: true});
-
-    const octokit = new github.GitHub(token);
     const context = github.context;
-
-    const filterBodies = (issue: any) => issue.body.includes('ZD-');
-    const mapZeds = (issue: any) => issue.body.match(/ZD-(\d+)/g);
-
-    const issue: Array<string> = await octokit.issues
-      .get({
-        ...context.repo,
-        issue_number: context.issue.number
-      })
-      .then(result => mapZeds(result.data));
-
-    const comments: Array<string> = await octokit.issues
-      .listComments({
-        ...context.repo,
-        issue_number: context.issue.number
-      })
-      .then(result => result.data.filter(filterBodies).map(mapZeds));
-
-    const zeds = issue.concat(comments.flat());
+    const zeds = await query();
     console.log('ZEDS', zeds);
 
     core.setOutput('zeds', JSON.stringify(zeds));
@@ -33,4 +17,50 @@ export async function zeds(): Promise<void> {
   } catch (error) {
     core.setFailed(error.message);
   }
+}
+
+export async function query(): Promise<Array<string>> {
+  const token = core.getInput('token', {required: true});
+
+  const octokit: github.GitHub = new github.GitHub(token);
+  const context = github.context;
+  let zeds = await bodies(octokit);
+
+  return [
+    ...new Set(
+      zeds
+        .map((issue: any) => {
+          let matches: Array<string> = [];
+
+          const shorthands = [...issue.matchAll(ZD_REGEX)];
+          matches = matches.concat(shorthands.map((m: any) => m[0]));
+
+          const urls = [...issue.matchAll(ZD_URL_REGEX)];
+          matches = matches.concat(urls.map((m: any) => `ZD-${m[1]}`));
+
+          return matches;
+        })
+        .flat()
+    ).keys()
+  ];
+}
+
+export async function bodies(octokit: github.GitHub): Promise<Array<string>> {
+  const context = github.context;
+
+  const issue: Array<string> = await octokit.issues
+    .get({
+      ...context.repo,
+      issue_number: context.issue.number
+    })
+    .then(result => [result.data.title, result.data.body]);
+
+  const comments: Array<string> = await octokit.issues
+    .listComments({
+      ...context.repo,
+      issue_number: context.issue.number
+    })
+    .then(result => result.data.map(i => i.body));
+
+  return comments.concat(issue);
 }
